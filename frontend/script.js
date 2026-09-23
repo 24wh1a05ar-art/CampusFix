f72a089b5d8a6e15bd12a91c4b0159b543d24bc6
Kavya Pasunuri <24wh1a05ar@bvrithyderabad.edu.in>
2026-09-23 07:10:06 +0530

    Connect frontend to live backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = "https://campusfix-ijod.onrender.com";
 
 const user = JSON.parse(localStorage.getItem("user"));
 const token = localStorage.getItem("token");
